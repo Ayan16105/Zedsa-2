9a3153150ad131ace61e0459c4c77054d985914a
Ayan khan <144985920+Ayan16105@users.noreply.github.com>
2024-06-04 17:32:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".ripple").ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.09,
+});
+});
+
+(document).ready(function(){
+    
+ $("ripple").ripples("drop", 29, 12, 10, 20);
+});
